8a90a7ed491d911251ff618438e0fefb6166987b
hieurobert <hieurobert@gmail.com>
2023-12-19 14:49:03 +0100

    task 7

--- src/main/java/task7/WordCounter.java
+++ src/main/java/task7/WordCounter.java
@@ -1,43 +1,44 @@
-//package task7;
-//
-//import org.apache.commons.io.FileUtils;
-//import org.apache.commons.lang3.StringUtils;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.util.HashSet;
-//import java.util.List;
-//import java.util.Set;
-//
-//public class WordCounter {
-//
-//    public static void main(String[] args) {
-//        // Read text from a file
-//        File inputFile = new File("sample.txt");
-//        File outputFile = new File("sampleOutput.txt");
-//
-//        try {
-//            List<String> lines = FileUtils.readLines(inputFile, "UTF-8");
-//
-//            // Combine lines into a single string
-//            String content = StringUtils.join(lines, " ");
-//
-//            // Split text into words
-//            String[] words = content.split("\\s+");
-//
-//            // Count unique words
-//            Set<String> uniqueWords = new HashSet<>();
-//            for (String word : words) {
-//                if (!word.isEmpty()) {
-//                    uniqueWords.add(word.toLowerCase()); // Consider words case-insensitively
-//                }
-//            }
-//
-//            // Write the count of unique words to the output file
-//            FileUtils.writeStringToFile(outputFile, "Number of unique words: " + uniqueWords.size(), "UTF-8");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-//
+package task7;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//Read the text from the file and calculate the numbers of the unique words. Write the result to the file.
+//The main requirement is: using StringUtils and FileUtils to implement it with minimum lines of code.
+public class WordCounter {
+
+    public static void main(String[] args) {
+
+        try {
+            // Read text from a file
+            List<String> lines = FileUtils.readLines(new File("sample.txt"));
+
+
+            // Combine lines into a single string
+            String content = StringUtils.join(lines, " ");
+
+            // Split text into words
+            String[] words = content.split("\\s+");
+
+            // Count unique words
+            Set<String> uniqueWords = new HashSet<>();
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    uniqueWords.add(word.toLowerCase()); // Consider words case-insensitively
+                }
+            }
+
+            // Write the count of unique words to the output file
+            FileUtils.writeStringToFile(new File("output.txt"), "Number of unique words: " + uniqueWords.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
